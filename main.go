bb3f6eb82921f96c32c4cfb4202f1bfcf11f2492
Emerald <emmyj1192@gmail.com>
2026-03-20 16:07:44 +0100

    Introducing Condition to make app more dynamic

--- main.go
+++ main.go
@@ -11,23 +11,25 @@ import (
 func main(){
 	//Constant Declaration
 	const conferenceName string = "Go Conference";
+	const conferenceTickets int = 50;
 
     //Variable declaration
-	conferenceTickets := 50;
 	var firstName string;
 	var lastName string;
 	var email string;
-	var ticketBook int;
-	var remainingTickets int;
+	var ticketBook uint;
+
+	remainingTickets:= 50;
 	bookings := [] string {};
 
 
 
 	for{
 		//Welcome message
-		fmt.Printf("Welcome to %v\n", conferenceName)
 		fmt.Printf("==================================================================\n")
-		fmt.Printf("We have total of %v tickets, and we are here at your service...\n", conferenceTickets)
+		fmt.Printf("Welcome to %v Application\n", conferenceName)
+		fmt.Printf("==================================================================\n")
+		fmt.Printf("We have total of %v tickets, with %v remaining\n",conferenceTickets, remainingTickets)
 	
 		//taking user input
 		//user input for first name
@@ -45,29 +47,40 @@ func main(){
 		//user input for ticket booking
 		fmt.Printf("How many ticktes would you like to book? \n")
 		fmt.Scan(&ticketBook)
-	
+
+		//if user inputs invalid value
+		if ticketBook > uint(remainingTickets) {
+			//prompt user the available ticket 
+			fmt.Printf("You have entered a wrong amount of tickets\nas we do not have up to that at the moment.\n")
+			fmt.Printf("You can purchase within %v\n", remainingTickets)
+			continue			
+		}
+
 		//calculating remaining tickets
-		remainingTickets = conferenceTickets - ticketBook;
+		remainingTickets = remainingTickets - int(ticketBook);
 
 		//Final Output
 		fmt.Printf("Thank you, %v for booking %v ticket. A mail will be sent to %v\n", firstName, ticketBook, email)
 		fmt.Printf("We have %v more ticket remaining for the %v\n", remainingTickets, conferenceName)
 
-
 		//list of bookings 
 		bookings = append(bookings, firstName + " " + lastName);
-
+		//security layer for bookings first name only 
 		firstNames := [] string {};
-
 		for _, booking := range bookings{
 			names := strings.Fields(booking)
 			//print out users first name only
 			firstNames = append(firstNames, names[0])
 		}
-
+		//print out bookings first name only 
 		fmt.Printf("The first name of bookings: %v\n", firstNames)
 
-		conferenceTickets -= ticketBook;
+
+		if remainingTickets == 0 {
+			//end the application if this condition is true
+			fmt.Printf("You have reach the end of our bookings") 
+			break
+		}
 		fmt.Printf("==================================================================\n")
 	}
 }
